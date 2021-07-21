5aa8a936e84ecf324d3c0575ed25ce92ee8c3bd4
Kayla Phelps <kmphelps822@gmail.com>
2021-07-21 15:26:58 -0700

    commit tree 0ae457075c6c3c2974b5f9624e7444c5ab51c645

---
+++ index.js
@@ -0,0 +1,10 @@
+let numA = 2000;
+let numB = 100;
+const divide = (numA = 2000, numB = 100) => numA / numB;   
+
+let x = 2;
+const square = x => x * x;
+
+let a = 3;
+let b = 4;
+const add = (a, b) => a + b;
